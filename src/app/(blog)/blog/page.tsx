54479d256568de63b406f7256ace4554b174e0a1
Benjamin <ben@ctos.sh>
2025-03-10 05:57:19 -0500

    chore: add blog intro

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -8,7 +8,7 @@ const BlogPostList = dynamic(() => import('@/app/components/BlogPostList'));
 
 export const metadata: Metadata = {
   title: 'Blog | Benjamin Ryan',
-  description: 'Blog posts about web development, technology, and more.',
+  description: 'Posts about web development, technology, and more.',
 };
 
 export default async function BlogPage() {
@@ -20,7 +20,11 @@ export default async function BlogPage() {
         <div className="bg-white dark:bg-transparent">
           <div className="prose prose-lg dark:prose-invert max-w-none prose-headings:font-mono prose-headings:tracking-tighter p-6 md:p-10 flex-grow">
             <div className="relative z-10 before:absolute before:inset-0 before:bg-[url('/atlas_blog.png')] before:bg-repeat before:opacity-10 before:dark:opacity-5 before:-z-10">
-              <h1 className="font-mono text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter mb-6">Blog Posts</h1>
+              <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter mb-6">ctos.sh - Blog</h1>
+              <p>
+                Hey, I'm Ben! Welcome to my blog! I write about web development, technology, and other topics I'm
+                passionate about.
+              </p>
             </div>
 
             <BlogPostList posts={posts} />
